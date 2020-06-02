eb9315341bdbb676975ad561e97e8a0447b8529b
BJKill <brandon.j.kill@gmail.com>
2020-06-02 17:24:23 -0400

    Added first functions from lecture

--- Scripts/Week2Lecture.R
+++ Scripts/Week2Lecture.R
@@ -87,9 +87,61 @@ for(i in 1:100) {
 
 for(i in 1:100) {
   print(i)
-  if(i > 20) {  ##stops after 20 iterations but prints '21'
+  if(i > 20) {  ##stops after 20 iterations but prints '21' bc print is before break
     break
   }
 }
+
+##funtion time!
+add2 <- function(x,y) {
+  x + y
+}
+add2(3,5)  #correctly returns 8
+
+above10 <- function(x) {
+  use <- x > 10
+  x[use]
+}
+
+sue <- c(65, 2, 1000000, -10, 9)
+above10(sue)
+
+aboven <- function(x,n) {
+  use <- x > n
+  x[use]
+}
+
+aboven(sue,0)
+aboven(sue,5)
+
+t<-1:20
+aboven(t,12)
+
+#finding the mean of columns in a data frame or matrix
+columnmean <- function(y, removeNA = TRUE) {
+  nc <- ncol(y)             #dummy variable for # cols
+  means <- numeric(nc)      #create empty numeric vector
+  for(i in 1:nc) {
+    means[i] <- mean(y[,i], na.rm = removeNA)
+  }
+  print(means)
+}
+
+columnmean2 <- function(y, removeNA = TRUE) {
+  means <- 1:ncol(y)
+  for(i in 1:ncol(y)) {
+    means[i] <- mean(y[,i], na.rm = removeNA)
+  }
+  print(means)
+}
+
+mat <- matrix(1:30, nrow = 5, ncol = 6)
+
+columnmean(mat)
+columnmean2(mat)
+
+columnmean(airquality)
+columnmean2(airquality)
+
 
 
