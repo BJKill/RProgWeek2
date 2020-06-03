2002dcc2728e925cf1fbecf92b36d6fb34b84269
BJKill <brandon.j.kill@gmail.com>
2020-06-03 14:50:16 -0400

    more functions plus scoping rules (assigning values to symbols and referencing them)

--- Scripts/Week2Lecture.R
+++ Scripts/Week2Lecture.R
@@ -143,5 +143,120 @@ columnmean2(mat)
 columnmean(airquality)
 columnmean2(airquality)
 
+#all expressions return same value although some better than others for clarity
+mydata <- rnorm(100)
+sd(mydata)
+sd(x = mydata)
+sd(x = mydata, na.rm = FALSE)
+sd(na.rm = FALSE, mydata)
+sd(na.rm = FALSE, x = mydata)
+
+#nothing worked here
+args(lm)
+lm(data = mydata, y - x, model = FALSE, 1:100)
+lm(y - x, data = mydata, 1:100)
+
+
+f<-function() {
+  cat("Hello, world!\n")
+}
+f()
+
+f <- function(a, b = 1, c = 2, d = NULL) {
+}
+
+#lazy evaluation. The function never uses b, so no input is fine
+f <- function(a,b) {
+  a^2
+}
+f(2)
+
+#will exicute until error, so a prints just fine
+f <- function(a,b) {
+  print(a)
+  print(b)
+}
+f(45)
+f(45,54)
+
+#using '...'
+myplot <- function(x, y, type = "1", ...) {
+  plot(x, y, type = type, ...)
+}
+
+args(paste)
+args(cat)
+
+##if '...' comes first, everything else after must be explicitly matched
+
+lm <- function(x) {x * x}
+lm
+search()
+rm(ls)
+rm(z) ##for next function
+
+f <- function(x,y) {
+  x^2 + y/z
+}
+
+f(2,4)  #error because no 'z' in global environment
+z = 2
+f(2,4)  #although no z input, it found it in glob env
+
+
+make.power <- function(n) {
+  pow <- function(x) {
+    x^n
+  }
+  pow
+}
+
+cube <- make.power(3)
+cube(3)    #becomes a function and asks for x
+square <- make.power(2)
+square(3)
+
+ls(environment(cube))
+get("n", environment(cube))
+ls(environment(square))
+get("n", environment(square))
+
+y<-10
+f<-function(x) {
+  y <- 2
+  y^2 + g(x)   #g(x) = x * 10 bc y=10 in the environment g was defined in   
+}
+
+g<-function(x) {  
+  x*y #Free variables in R functions take values from the environment in which the function is defined (10)
+}
+
+f(3) #returns 34
+
+rm(y) #take 'y' out of global environment
+
+g <- function(x) {
+  a <- 3
+  x+a+y
+}
+
+g(2) #returns error. no 'y' in global environment
+y<-3
+g(2) #ta daaaaa
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
