6e31382d1ffb725b2f0a6d71cb784fed96a74ccb
BJKill <brandon.j.kill@gmail.com>
2020-06-03 23:01:21 -0400

    Completed Swirl section 14 on Dates and Times

--- Scripts/Week2Swirl.R
+++ Scripts/Week2Swirl.R
@@ -90,23 +90,42 @@ all(ints > 0)
 
 
 
+##SWIRL Section 14: Dates and Times
+swirl()
+BJKill
+1
+14
 
+d1 <- Sys.Date()
+class(d1)
+unclass(d1)
+d1
 
+d2 <- as.Date("1969-01-01")
+unclass(d2)                  #returns '-365'
 
+t1 <- Sys.time()
+t1
+class(t1)
+unclass(t1)
 
+t2 <- as.POSIXlt(Sys.time())
+class(t2)
+t2                          #prints same style as t1
+unclass(t2)                 #prints a list
+str(unclass(t2))
+t2$min
 
+weekdays(d1)                #prints Wednesday (today)
+months(t1)                  #prints June (this month)
+quarters(t2)                #prints Q2 (this quarter)
 
+t3 <- "October 17, 1986 08:24"
+t4 <- strptime(t3, "%B %d, %Y %H:%M")
+t4
+class(t4)
 
-
-
-
-
-
-
-
-
-
-
-
-
+Sys.time() > t1                            #prints TRUE bc the time now is later than it was before
+Sys.time() - t1                            #prints 'Time difference of 8.783564 min'
+difftime(Sys.time(), t1, units = 'days')   #prints 'Time difference of 0.006855525 days'
 
