3e833d99fdb63b36759c500817b8e278602b28da
BJKill <brandon.j.kill@gmail.com>
2020-06-06 11:42:30 -0400

    All of the Swirl section over functions plus beggining of week2 quiz bc whoops

---
+++ Scripts/Week2Quiz.R
@@ -0,0 +1,29 @@
+### WEEK 2 QUIZ
+
+cube <- function(x,n) {
+  x^3
+}
+
+cube(3)
+
+
+x <- 1:10
+if(x > 5) {   # Produces error bc x is vector len=10 while if only tests a single logical statement
+  x <- 0
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
