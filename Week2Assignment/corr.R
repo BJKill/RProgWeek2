963d4a9f502e41ac637b14e580608244c4a2d317
BJKill <brandon.j.kill@gmail.com>
2020-06-06 19:30:49 -0400

    progress on part 3

--- Week2Assignment/corr.R
+++ Week2Assignment/corr.R
@@ -2,21 +2,40 @@
 ##         between sulfate and nitrate for monitor locations that meet stated threshold
 
 corr <- function(directory, threshold = 0) {
-  setwd("Data")
-  files_full <- list.files(directory, full.names = TRUE)
-  
-  #build data frame from files
-  dat <- data.frame()
-  for(i in seq_along(files_full)) {
-    dat <- rbind(dat, read.csv(files_full[i]))
-  }
-  setwd(oldwd)
-  comp_sets <- complete(directory)
-  good_sets <- comp_sets$id[which(comp_sets$nobs >= threshold)]
-  good_data <- dat[[good_sets]]
-  print(class(good_data))
-  setwd(oldwd)
-  head(good_data)
+    files_full <- list.files(directory, full.names = TRUE)
+    dat <- data.frame()
+    for(i in seq_along(files_full)) {
+      dat <- rbind(dat, read.csv(files_full[i]))
+    }
+    splitdata <- split(dat, dat$ID)
+    id <- 1:332
+    nobs <- vector()
+    for(i in seq_along(splitdata)) {
+      nobs[i] <- sum(complete.cases(splitdata[[i]]))
+    }
+    comp_sets <- data.frame("id" = as.numeric(id), "nobs" = as.numeric(nobs))
+    #str(comp_sets)
+    good_sets <- comp_sets$id[which(comp_sets$nobs >= threshold)]
+    #str(good_sets)
+    good_data <- splitdata[good_sets]
+    #str(good_data)
+    sulfvect <- vector()
+    nitrvect <- vector()
+    for(i in seq_along(good_data)) {
+      sulfvect <- c(sulfvect, good_data[[i]][["sulfate"]])
+      nitrvect <- c(nitrvect, good_data[[i]][["nitrate"]])
+    }
+    polmat <- cbind(sulfvect, nitrvect)
+    corvect <- vector()
+    corvect <- cor(polmat, use = "pairwise.complete.obs")
+    corvect
+    #str(sulfvect)
+    #str(nitrvect)
+    #corvect <- vector()
+    #for(i in seq_along(good_data)) {
+    #  corvect[i] <- cor(good_data[[i]][["nitrate"]], good_data[[i]][["sulfate"]])
+    #}
+    #corvect
 }
 
 
