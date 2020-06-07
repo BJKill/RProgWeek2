f12f0752576d0f0b37b35293db4d0d3a2ed4ea33
BJKill <brandon.j.kill@gmail.com>
2020-06-06 21:06:46 -0400

    Finished part3 of assignment

--- Week2Assignment/corr.R
+++ Week2Assignment/corr.R
@@ -14,28 +14,39 @@ corr <- function(directory, threshold = 0) {
       nobs[i] <- sum(complete.cases(splitdata[[i]]))
     }
     comp_sets <- data.frame("id" = as.numeric(id), "nobs" = as.numeric(nobs))
-    #str(comp_sets)
-    good_sets <- comp_sets$id[which(comp_sets$nobs >= threshold)]
-    #str(good_sets)
+
+    good_sets <- comp_sets$id[which(comp_sets$nobs > threshold)]
+
     good_data <- splitdata[good_sets]
-    #str(good_data)
-    sulfvect <- vector()
-    nitrvect <- vector()
-    for(i in seq_along(good_data)) {
-      sulfvect <- c(sulfvect, good_data[[i]][["sulfate"]])
-      nitrvect <- c(nitrvect, good_data[[i]][["nitrate"]])
-    }
-    polmat <- cbind(sulfvect, nitrvect)
+
+    sulflist <- list()
+    nitrlist <- list()
     corvect <- vector()
-    corvect <- cor(polmat, use = "pairwise.complete.obs")
-    corvect
-    #str(sulfvect)
-    #str(nitrvect)
-    #corvect <- vector()
-    #for(i in seq_along(good_data)) {
-    #  corvect[i] <- cor(good_data[[i]][["nitrate"]], good_data[[i]][["sulfate"]])
-    #}
-    #corvect
+    corvect2 <- vector()
+    
+    if(length(good_data) > 0) {
+      for(i in 1:length(good_data)) {
+        sulflist[[i]] <- good_data[[i]][["sulfate"]]
+        nitrlist[[i]] <- good_data[[i]][["nitrate"]]
+        corvect[i] <- cor(x = sulflist[[i]], y = nitrlist[[i]], use = "pairwise.complete.obs")
+      }
+      corvect2 <- corvect[!is.na(corvect)]
+    }
+    as.numeric(corvect2)
 }
 
-
+## Test code
+## 
+## cr1 <- corr("specdata", 150)
+## summary(cr1)
+## 
+## cr2 <- corr("specdata", 400)
+## summary(cr2)
+## 
+## cr3 <- corr("specdata", 5000)
+## summary(cr3)
+## length(cr3)
+## 
+## cr4 <- corr("specdata")
+## summary(cr4)
+## length(cr4)
